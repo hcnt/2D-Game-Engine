2c25c7b0b1477987480ddcd272b640bc10758c40
pttx <jacek.kusmierczyk@wp.pl>
2018-04-18 00:19:24 +0200

    add item components

--- src/com/pttx/snakeGame/Game.java
+++ src/com/pttx/snakeGame/Game.java
@@ -3,12 +3,15 @@ package com.pttx.snakeGame;
 import com.pttx.game_engine.AbstractGame;
 import com.pttx.game_engine.GameContainer;
 import com.pttx.game_engine.Renderer;
+import com.pttx.game_engine.gfx.Image;
 
 import java.util.ArrayList;
 
 public class Game implements AbstractGame {
-    private int GridNumber= 16;
-    private Snake snake = new Snake(GridNumber);
+    private int gridNumber = 16;
+    private Snake snake = new Snake(gridNumber);
+    private ArrayList<ItemComponent> items = new ArrayList<>();
+    private Image itemImage = new Image("/redSquare.png");
 
     public Game(){
     }
@@ -16,12 +19,18 @@ public class Game implements AbstractGame {
     @Override
     public void update(GameContainer g, double delta) {
         snake.updateSnake(g);
+        if (items.size() < 1){
+            items.add(new ItemComponent((int)(Math.random()*gridNumber),(int)(Math.random()*gridNumber)));
+        }
     }
 
     @Override
     public void render(GameContainer g, Renderer r) {
-        r.drawGrid(GridNumber,GridNumber,0xff436325);
+        r.drawGrid(gridNumber, gridNumber,0xff436325);
         snake.drawSnake(r,g.getWidth());
+        for(Component item : items){
+            item.drawComponent(r,itemImage,g.getHeight()/gridNumber);
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/com/pttx/snakeGame/ItemComponent.java
@@ -0,0 +1,14 @@
+package com.pttx.snakeGame;
+
+import com.pttx.game_engine.gfx.Image;
+
+public class ItemComponent extends Component{
+    int value = 1;
+
+    ItemComponent(int x, int y){
+        super(x,y);
+    }
+
+
+}
+
