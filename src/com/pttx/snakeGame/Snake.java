301bf6d9bfb7aaa645dcc112ba74f4c05a6ea048
pttx <jacek.kusmierczyk@wp.pl>
2018-04-17 22:36:08 +0200

    add snake turning

--- src/com/pttx/snakeGame/Snake.java
+++ src/com/pttx/snakeGame/Snake.java
@@ -4,26 +4,32 @@ import com.pttx.game_engine.GameContainer;
 import com.pttx.game_engine.Renderer;
 import com.pttx.game_engine.gfx.Image;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class Snake {
     private ArrayList<Component> components;
-    private Image body = new Image("/test.png");
+    private Image body = new Image("/square.png");
     private int xSpeed = 0;
     private int ySpeed = 1;
     private long updateTimer = System.nanoTime();
     private int gridNumber;
+    private boolean wasDirectionChanged = false;
 
     public Snake(int gridNumber){
         this.gridNumber = gridNumber;
         components = new ArrayList<>();
         components.add(new Component(5,5));
-        components.add(new Component(6,5));
+        components.add(new Component(5,6));
+        components.add(new Component(5,7));
     }
     public void updateSnake(GameContainer g){
-        if (System.nanoTime() - updateTimer > 1000000000) {
+        changeDirection(g);
+        if (System.nanoTime() - updateTimer > 100000000) {
+            wasDirectionChanged = false;
             updateTimer = System.nanoTime();
-            for (int i = 1; i<components.size(); i++) {
+            System.out.println(components.size());
+            for (int i = components.size()-1; i>=1; i--) {
                 components.get(i).x = components.get(i-1).x;
                 components.get(i).y = components.get(i-1).y;
             }
@@ -31,12 +37,32 @@ public class Snake {
             components.get(0).y += ySpeed;
         }
     }
-
+    public void changeDirection(GameContainer g){
+        if(g.getInput().isKeyDown(KeyEvent.VK_DOWN) && ySpeed != -1 && !wasDirectionChanged){
+            ySpeed = 1;
+            xSpeed = 0;
+            wasDirectionChanged = true;
+        }
+        if(g.getInput().isKeyDown(KeyEvent.VK_UP) && ySpeed != 1 && !wasDirectionChanged){
+            ySpeed = -1;
+            xSpeed = 0;
+            wasDirectionChanged = true;
+        }
+        if(g.getInput().isKeyDown(KeyEvent.VK_LEFT) && xSpeed != 1 && !wasDirectionChanged){
+            xSpeed = -1;
+            ySpeed = 0;
+            wasDirectionChanged = true;
+        }
+        if(g.getInput().isKeyDown(KeyEvent.VK_RIGHT) && xSpeed != -1 && !wasDirectionChanged){
+            xSpeed = 1;
+            ySpeed = 0;
+            wasDirectionChanged = true;
+        }
+    }
     public void drawSnake(Renderer r,int windowWidth){
         int componentSize = windowWidth / gridNumber;
         for(int i = 0; i<components.size(); i++){
             r.drawImage(body,components.get(i).x*componentSize,components.get(i).y*componentSize);
         }
     }
-
 }
