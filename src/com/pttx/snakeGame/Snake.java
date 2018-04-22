140019370006fb22561c95e5f5b6af34afc4ad11
pttx <jacek.kusmierczyk@wp.pl>
2018-04-22 16:03:21 +0200

    add rendering images only at turning components

--- src/com/pttx/snakeGame/Snake.java
+++ src/com/pttx/snakeGame/Snake.java
@@ -19,7 +19,7 @@ public class Snake {
     private boolean wasDirectionChanged = false;
     private ArrayList<Direction> directionsOfHead = new ArrayList<>();
     private ArrayList<Direction> directionsOfBody = new ArrayList<>();
-    private int deltaTime = 166000000;
+    private int deltaTime = 1000000000;//166000000;
 
     public enum Direction{
         UP(0,-1),DOWN(0,1),RIGHT(1,0),LEFT(-1,0);
@@ -63,12 +63,21 @@ public class Snake {
 
     public void updateSnake(GameContainer g){
         changeDirection(g);
+        setComponentOnTurn();
         if (System.nanoTime() - updateTimer > deltaTime) {
             updateTimer = System.nanoTime();
+            for (int i = components.size()-1; i>=1; i--) {
+                if (components.get(i - 1).isOnTurn()) {
+                    components.get(i).setOnTurn(true);
+                    components.get(i - 1).setOnTurn(false);
+                }
+            }
+            components.get(components.size()-1).setOnTurn(false);
             for (int i = components.size()-1; i>=1; i--) {
                 components.get(i).x = components.get(i-1).x;
                 components.get(i).y = components.get(i-1).y;
             }
+
             components.get(0).x += directionsOfHead.get(0).xSpeed;
             components.get(0).y += directionsOfHead.get(0).ySpeed;
 
@@ -79,7 +88,7 @@ public class Snake {
             if(directionsOfHead.size() > 1) {
                 directionsOfHead.remove(0);
             }
-            //System.out.println(directionsOfBody.get(0));
+
         }
     }
     public void changeDirection(GameContainer g){
@@ -102,20 +111,36 @@ public class Snake {
     public void drawSnake(Renderer r,int windowWidth){
         int componentSize = windowWidth / gridNumber;
         int xSpeed;
-        for(int i = 1; i<components.size()-1; i++){
-            xSpeed = Math.abs(directionsOfBody.get(components.size()-2-i).xSpeed);
-            components.get(i).drawComponentTile(r,body,componentSize,xSpeed,0);
-
-        }
-        xSpeed = Math.abs(directionsOfHead.get(0).xSpeed);
-        head.drawComponentTile(r, body, componentSize,xSpeed,0);
+            for (int i = 1; i < components.size() - 1; i++) {
+                if (components.get(i).isOnTurn()) {
+                    components.get(i).drawComponentTile(r, body, componentSize, 1, 1);
+                } else {
+                    xSpeed = Math.abs(directionsOfBody.get(components.size() - 2 - i).xSpeed);
+                    components.get(i).drawComponentTile(r, body, componentSize, xSpeed, 0);
+                }
+            }
+            if (components.get(0).isOnTurn()) {
+                components.get(0).drawComponentTile(r, body, componentSize, 1, 1);
+            } else {
+                xSpeed = Math.abs(directionsOfHead.get(0).xSpeed);
+                head.drawComponentTile(r, body, componentSize, xSpeed, 0);
+            }
         smoothRenderComponent(r,componentSize,headImage,tail.x,tail.y, directionsOfBody.get(0),System.nanoTime() - updateTimer,0);
         smoothRenderComponent(r,componentSize,headImage,head.x,head.y, directionsOfHead.get(0),System.nanoTime() - updateTimer,2);
-
     }
     public void smoothRenderComponent(Renderer r, int componentSize, Image image, int x, int y, Direction direction, long timePassed,int offset) {
         float xOffset = (x * componentSize + ((direction.xSpeed * (timePassed / (float)deltaTime))*componentSize) - direction.xSpeed*offset);
         float yOffset = (y * componentSize + ((direction.ySpeed * (timePassed / (float)deltaTime))*componentSize) - direction.ySpeed*offset);
         r.drawImage(image, (int)xOffset, (int)yOffset);
     }
+    public void setComponentOnTurn(){
+        if(components.get(0).x + directionsOfHead.get(0).xSpeed != components.get(1).x && components.get(0).y + directionsOfHead.get(0).ySpeed != components.get(1).y){
+            components.get(0).setOnTurn(true);
+        }
+    }
+    public void drawComponentOnTurn(int index){
+
+    }
+
+
 }
